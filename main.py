ceeb33e2a6c32044cf2df50142cc78be05e80896
jaggaer-sfelaco <90247417+jaggaer-sfelaco@users.noreply.github.com>
2025-04-11 18:16:05 +0200

    Simple Reflection agent

---
+++ main.py
@@ -0,0 +1,56 @@
+from typing import List, Sequence
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.graph import END, MessageGraph
+
+from chains import generate_chain, reflect_chain
+
+
+REFLECT = "reflect"
+GENERATE = "generate"
+
+
+def generation_node(state: Sequence[BaseMessage]) -> List[BaseMessage]:
+    return generate_chain.invoke({"messages": state})
+
+
+def reflection_node(messages: Sequence[BaseMessage]) -> List[BaseMessage]:
+    res = reflect_chain.invoke({"messages": messages})
+    return [HumanMessage(content=res.content)]
+
+
+def should_continue(state: List[BaseMessage]):
+    if len(state) > 2:
+        return END
+    return REFLECT
+
+if __name__ == "__main__":
+    print("Hello LangGraph")
+    inputs = HumanMessage(content="""Make this tweet better:"
+                                    @LangChainAI
+            — newly Tool Calling feature is seriously underrated.
+
+            After a long wait, it's  here- making the implementation of agents across different models with function calling - super easy.
+
+            Made a video covering their newest blog post
+
+                                  """)
+    
+    
+    builder = MessageGraph()
+    builder.add_node(GENERATE, generation_node)
+    builder.add_node(REFLECT, reflection_node)
+    builder.set_entry_point(GENERATE)
+
+    builder.add_conditional_edges(GENERATE, should_continue)
+    builder.add_edge(REFLECT, GENERATE)
+
+    graph = builder.compile()
+    print(graph.get_graph().draw_mermaid())
+    graph.get_graph().print_ascii()
+    
+    response = graph.invoke(inputs)
+    print(response[len(response) - 1].content)
